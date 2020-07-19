7c76e7a2a0257241f4dbbd1b7d174df852a9711d
YongHwi Kim <yh0921k@gmail.com>
2020-07-20 01:30:25 +0900

    [add] 스프링 시큐리티 설정 전 구글 로그인 준비

---
+++ src/main/java/com/spring/book/springboot/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.book.springboot.domain.user;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // 이메일을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지 여부 판단
+    Optional<User> findByEmail(String email);
+}
